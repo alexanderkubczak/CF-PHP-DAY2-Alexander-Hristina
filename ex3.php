83609e7e3089b11bdbddd509c8be7daf6668dc11
Hristina Dakic <hristina.dakic@gmail.com>
2019-09-10 11:04:46 +0200

    does ex3

---
+++ ex3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ex3</title>
+</head>
+<body>
+	<?php  
+		function divide($val1, $val2)
+		{
+			$divided = $val1/$val2;
+			echo "The result is: $divided";
+		}
+		divide(10,2);
+	?>
+
+
+</body>
+</html>
